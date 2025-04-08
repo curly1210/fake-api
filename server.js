41d83b3658584e8fbfdb682312b4a848d54d085d
curly1210 <1210.curly@gmail.com>
2025-04-08 10:25:03 +0700

    fix:cors

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // server.js
 import jsonServer from "json-server";
 import auth from "json-server-auth";
-// import cors from "cors";
+import cors from "cors";
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 // Middleware
 app.db = router.db;
-// app.use(cors());
+app.use(cors());
 app.use(jsonServer.bodyParser);
 app.use(auth);
 app.use(router);
