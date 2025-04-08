1ec1c55356f24b9fd21c7c3ea8e67fec8ef4e03e
curly1210 <1210.curly@gmail.com>
2025-04-08 10:13:40 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+import jsonServer from "json-server";
+import auth from "json-server-auth";
+// import cors from "cors";
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+// Middleware
+app.db = router.db;
+// app.use(cors());
+app.use(jsonServer.bodyParser);
+app.use(auth);
+app.use(router);
+
+// Khởi chạy server
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
